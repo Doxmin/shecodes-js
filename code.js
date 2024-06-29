4fd732c045784ae0367b6bbcff3f820131ce3604
ghostlyphobic <novaacat@gmail.com>
2024-06-29 12:36:25 -0400

    changes to code.js

--- code.js
+++ code.js
@@ -1,5 +1,35 @@
-function isWindy(){
-    alert("It is windy");
-};
-isWindy();
-isWindy();
+// function isWindy(){
+//     alert("It is windy");
+// };
+// isWindy();
+// isWindy();
+
+// greetUser(firstName) {
+//     alert(`Welcome ${name}!`);
+// }
+
+// function welcomeUser(temperature, name) {
+// if (temperature) > 15) {
+//     alert("It is a warm day" + name);
+// } else {
+//   alert("It is a cold day, " + name);
+// }
+// }
+
+// welcomeUser(10,)
+
+
+//Challange two
+
+function itsWindy(speed){
+   if(speed > 5){
+    alert("it is windy");
+   }
+ 
+ else {
+        alert("it is not windy");
+}
+}
+
+itsWindy(5);
+itsWindy(10);
