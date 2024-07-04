f2d6c8184df6b29ded09b682f9956532c3de0195
ghostlyphobic <novaacat@gmail.com>
2024-07-04 18:15:33 -0400

    finished js homework

--- code.js
+++ code.js
@@ -33,3 +33,39 @@
 
 // itsWindy(5);
 // itsWindy(10);
+
+
+
+ // Function to handle vacation planning
+ function planVacation() {
+    // Prompt the user for destination
+    var destination = prompt("Where would you like to go on vacation?");
+    
+    // Prompt the user for trip duration
+    var tripDuration = prompt("How long is your trip (in days)?");
+    tripDuration = parseInt(tripDuration); // Convert input to integer
+    
+    // Prompt the user if they want to go on excursions
+    var wantExcursions = confirm("Would you like to go on excursions?");
+    
+    // Display summary based on user inputs
+    var summary = "Vacation Summary:\n\n";
+    summary += "Destination: " + destination + "\n";
+    summary += "Trip Duration: " + tripDuration + " days\n";
+    summary += "Excursions: " + (wantExcursions ? "Yes" : "No") + "\n";
+    
+    alert(summary);
+    
+    // Additional logic based on user preferences
+    if (tripDuration >= 7) {
+      alert("That sounds like a nice long vacation!");
+    } else {
+      alert("Short and sweet! Enjoy your trip!");
+    }
+    
+    if (wantExcursions) {
+      alert("Don't forget to plan some exciting excursions!");
+    } else {
+      alert("Relaxing vacation ahead!");
+    }
+  }
